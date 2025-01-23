63ca537e5b8afb31665f1b42f910f423d379ae76
Ricardo <ricardop5395@gmail.com>
2025-01-23 13:28:14 -0400

    actualizando

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 #pip install -r requirements.txt
 
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src:/opt/render/project/src/PST-IV/apps
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
